9e008e0edb6bbbf3beae05aadf3cbc9a2c870069
shariar-duke <shariarmahmud225@gmail.com>
2024-06-19 14:05:20 +0600

    canges in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,17 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      container: {
+				center: true,
+				padding: "1.25rem",
+			},
+			colors: {
+				primary: '#00D991'
+			},
+    },
   },
   plugins: [],
 }
