2770d581680861140a9b8a346fe87698dc37b60b
shariar-duke <shariarmahmud225@gmail.com>
2024-06-19 18:36:53 +0600

    sidebar and movieList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import MovieList from "./components/cine/MovieList";
 
 export default function App() {
   return (
     <div className="h-[100vh]">
       <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.8rem]">
+          <SideBar/>
+          <MovieList/>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+import Trending from "../assets/icons/trending.svg";
+import NewRelease from "../assets/icons/newRelease.svg";
+import CommingSoon from "../assets/icons/commingSoon.svg";
+import Favourite from "../assets/icons/favourite.svg"
+import WatchLater from "../assets/icons/watchLater.svg"
+
+export default function SideBar() {
+  return (
+    <aside className="text-white">SideBar</aside>
+  )
+}
