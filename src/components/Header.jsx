a6d01653308b98387b483981b50f26bbf39cfc41
shariar-duke <shariarmahmud225@gmail.com>
2024-06-19 14:24:52 +0600

    shifting images form public to src-> assests

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Moon from "/icons/moon.svg";
+import Sun from "/icons/sun.svg";
+import Logo from "/icons/logo.svg";
+ import Ring from "/icons/ring"
+
+export default function Header() {
+  return (
+    <Header>Header</Header>
+  )
+}
