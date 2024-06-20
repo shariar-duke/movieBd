335dcce09d1ae8630f7e86cadafee3924e99314d
shariar-duke <shariarmahmud225@gmail.com>
2024-06-20 08:52:33 +0600

    height fixed

--- src/components/cine/MovieDetailsModal.jsx
+++ src/components/cine/MovieDetailsModal.jsx
@@ -4,7 +4,7 @@ export default function MovieDetailsModal({movie , setShowModal }) {
   const imgUrl = getImgUrl(movie?.cover);
   return (
     <div className='bg-black bg-opacity-30 z-10  fixed inset-0 flex items-center justify-center  text-white'>
-        <div className='grid lg:grid-cols-[3fr_1.5fr] max-w-[700px] h-[300px] bg-[#12141D] rounded-lg'>
+        <div className='grid lg:grid-cols-[3fr_1.5fr] max-w-[700px]  bg-[#12141D] rounded-lg'>
             <div className='p-[30px]  text-white'>
                 <h1 className='text-3xl font-extrabold '>{movie.title}</h1>
                 <p className='mt-1 text-sm text-[#9fa0a4]'>{movie.genre}</p>
@@ -20,7 +20,7 @@ export default function MovieDetailsModal({movie , setShowModal }) {
             <img
               src={imgUrl}
               alt="movie"
-              className="w-full object-cover h-[300px] rounded-lg"
+              className="w-full object-cover  rounded-lg"
             />
             </div>
         </div>
