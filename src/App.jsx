113aba980fe1e06d5990ad59e95f2704de234693
shariar-duke <shariarmahmud225@gmail.com>
2024-06-20 16:48:09 +0600

    Theme Context provided throught the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
-import MovieList from "./components/cine/MovieList"
+import MovieList from "./components/cine/MovieList";
 import { useState } from "react";
-import MovieDetailsModal from "./components/cine/MovieDetailsModal";
-import { MovieContext } from "./context"; // context k import korlam
+
+import { MovieContext, ThemeContext } from "./context"; // context k import korlam
 export default function App() {
-  const[cartData, setCartData] = useState([])
+  const [cartData, setCartData] = useState([]);
+  const [darkMode, setDarkMode] = useState(true) // theme control korar state
   return (
     <>
-      <MovieContext.Provider value={{cartData, setCartData}}>
-        <div className="h-full pb-[80px]">
-          <Header />
-          <main>
-            <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.8rem] mt-[10px]">
-              <SideBar />
-              <MovieList />
-            </div>
-          </main>
-        </div>
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        <ThemeContext.Provider value={{darkMode, setCartData}}>
+          <div className="h-full pb-[80px]">
+            <Header />
+            <main>
+              <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.8rem] mt-[10px]">
+                <SideBar />
+                <MovieList />
+              </div>
+            </main>
+          </div>
+        </ThemeContext.Provider>
       </MovieContext.Provider>
     </>
   );
