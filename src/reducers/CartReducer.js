11c4c9aa262468446077154e5c8c994b921cfea5
shariar-duke <shariarmahmud225@gmail.com>
2024-06-20 18:42:37 +0600

    reducer created with actions

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -1,14 +1,28 @@
-
 // reducer k initialize korar somoy ekta initial value dia initialze kore dite hoy
-const initialState ={
-    cartData:[]
-}
+const initialState = {
+  cartData: [],
+};
 
 // ekhane amra reducer function ta lekhbo
-const cartReducer = (state, action)=> 
-{
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        cartData: [...state.cartData, action.payload],
+      };
+      break;
 
-}
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cartData: state.cartData.filter(
+          (item) => item.id !== action.payload.id
+        ),
+      };
 
+    default:
+      return state;
+  }
+};
 
-export {initialState, cartReducer}
+export { initialState, cartReducer };
