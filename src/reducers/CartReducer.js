dc10860240047f2bf14745d0df161af1bd8a90f9
shariar-duke <shariarmahmud225@gmail.com>
2024-06-20 18:18:46 +0600

    initial state and reducer function created

---
+++ src/reducers/CartReducer.js
@@ -0,0 +1,14 @@
+
+// reducer k initialize korar somoy ekta initial value dia initialze kore dite hoy
+const initialState ={
+    cartData:[]
+}
+
+// ekhane amra reducer function ta lekhbo
+const cartReducer = (state, action)=> 
+{
+
+}
+
+
+export {initialState, cartReducer}
